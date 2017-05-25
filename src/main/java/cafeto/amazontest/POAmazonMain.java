43f5a1901671cd3d5361ae4358a4c39f0b722054
jpg117 <juanpablogarcia78@hotmail.com>
2017-05-25 06:35:54 -0500

    Adding test cases

--- src/main/java/cafeto/amazontest/POAmazonMain.java
+++ src/main/java/cafeto/amazontest/POAmazonMain.java
@@ -17,16 +17,26 @@ public class POAmazonMain {
     //Page elements
     WebElement myAccountDropDown;
     WebElement accountSettOpt;
+    WebElement usernameTextBox;
+    WebElement passwordTextBox;
+    WebElement oldUserRadioButton;
+    WebElement newUserRadioButton;
+    WebElement loginButton;
 
     //Locator
     By myAccountDropDownLoc = By.xpath("//*[@id=\"aws-nav-header-right\"]/div[2]/span");
     By accountSettOptLoc = By.xpath("//*[@id=\"aws-nav-dropdown-account\"]/ul/li[3]/a");
+    By usernameTextBoxLoc = By.xpath("//*[@id=\"ap_email\"]");
+    By passwordTextBoxLoc = By.xpath("//*[@id=\"ap_password\"]");
+    By oldUserRadioButtonLoc = By.xpath("//*[@id=\"ap_signin_existing_radio\"]");
+    By newUserRadioButtonLoc = By.xpath("//*[@id=\"ap_signin_create_radio\"]");
+    By loginButtonLoc = By.xpath("//*[@id=\"signInSubmit-input\"]");
 
     WebDriverWait wait;
 
     public POAmazonMain() {
         driver = WebDriverManager.getDriver();
-        wait = new WebDriverWait(driver,2);
+        wait = new WebDriverWait(driver,10);
     }
 
     public void goToAmazonMain(){
@@ -43,5 +53,35 @@ public class POAmazonMain {
         wait.until(ExpectedConditions.elementToBeClickable(accountSettOptLoc));
         accountSettOpt = driver.findElement(accountSettOptLoc);
         accountSettOpt.click();
+    }
+
+    public void typeUsername(String username){
+        wait.until(ExpectedConditions.elementToBeClickable(usernameTextBoxLoc));
+        usernameTextBox = driver.findElement(usernameTextBoxLoc);
+        usernameTextBox.sendKeys(username);
+    }
+
+    public void typePassword(String password){
+        wait.until(ExpectedConditions.elementToBeClickable(passwordTextBoxLoc));
+        passwordTextBox = driver.findElement(passwordTextBoxLoc);
+        passwordTextBox.sendKeys(password);
+    }
+
+    public void selectOldUser(){
+        wait.until(ExpectedConditions.elementToBeClickable(oldUserRadioButtonLoc));
+        oldUserRadioButton = driver.findElement(oldUserRadioButtonLoc);
+        oldUserRadioButton.click();
+    }
+
+    public void selectNewUser(){
+        wait.until(ExpectedConditions.elementToBeClickable(newUserRadioButtonLoc));
+        newUserRadioButton = driver.findElement(newUserRadioButtonLoc);
+        newUserRadioButton.click();
+    }
+
+    public void clickLoginButton(){
+        wait.until(ExpectedConditions.elementToBeClickable(loginButtonLoc));
+        loginButton = driver.findElement(loginButtonLoc);
+        loginButton.click();
     }
 }
--- src/test/java/cafeto/amazontest/AWSLoginTests.java
+++ src/test/java/cafeto/amazontest/AWSLoginTests.java
@@ -5,11 +5,46 @@ import static org.junit.Assert.*;
 
 public class AWSLoginTests {
 
-    @Test
-    public void loginAsAValidUser() {
+    //@Test
+    public void goToLoginForm() {
         POAmazonMain mainPage =  new POAmazonMain();
         mainPage.goToAmazonMain();
         mainPage.goToAmazonWSLogin();
+        //assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+
+    @Test
+    public void loginAsValidUserWithUserName() {
+        POAmazonMain mainPage =  new POAmazonMain();
+        mainPage.goToAmazonMain();
+        mainPage.goToAmazonWSLogin();
+        mainPage.selectOldUser();
+        mainPage.typeUsername("juanpablogarcia78@hotmail.com");
+        mainPage.typePassword("testcafeto123");
+        mainPage.clickLoginButton();
+        //assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+
+    //@Test
+    public void loginAsInvalidUser() {
+        POAmazonMain mainPage =  new POAmazonMain();
+        mainPage.goToAmazonMain();
+        mainPage.goToAmazonWSLogin();
+        mainPage.selectOldUser();
+        mainPage.typeUsername("correofalso@dominio.com");
+        mainPage.typePassword("testcafeto123");
+        mainPage.clickLoginButton();
+        //assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+
+    //@Test
+    public void loginAsNewUser() {
+        POAmazonMain mainPage =  new POAmazonMain();
+        mainPage.goToAmazonMain();
+        mainPage.goToAmazonWSLogin();
+        mainPage.selectNewUser();
+        mainPage.typeUsername("correofalso@dominio.com");
+        mainPage.clickLoginButton();
         //assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
 }
